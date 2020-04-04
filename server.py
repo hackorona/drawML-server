bbb7a40c8ddae9c153f578e9928a06189ee6707a
shimonBer <shimon.ber@gmail.com>
2020-04-04 23:45:48 +0300

    working server

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, session
 from flask_socketio import SocketIO
 from flask_socketio import join_room, emit
+import numpy as np
+import MLpart
 # ========= Project Constants =========== #
 
 ROOM_ID = 'roomId'
 JOIN = 'join'
 IDENTIFY = 'identify'
-USERNAME = 'username'
-IMAGE_MATRIX = 'matrix'
+PLAYER_NAME = 'playerName'
+DRAW_OBJECT = 'drawObject'
 GAME_OVER = 'gameOver'
 WINNER = 'winner'
 START = 'start'
@@ -20,19 +22,28 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 room_map = {}
 room_targets = {}
 
-ml_module = ''  # todo import ML module
+ml_module = MLpart.get_default_model()  # todo import ML module
 
 
-def find_room(user_name):
-    keys = [key for key, value in room_map.items() if user_name in value]
-    if len(keys) == 0:
+def find_room(room_name):
+    rooms = [key for key in room_map.keys() if room_name in key]
+    if len(rooms) == 0:
         return None
-    return keys[0]
+    return room_name[0]
+
+
+def create_matrix(draw_object):
+    size = (draw_object['width'], draw_object['width'])
+    matrix = np.zeros(size)
+    for line in draw_object['lines']:
+        for point in line['points']:
+            matrix[int(round(point['x'])), int(round(point['y']))] = 1
+    return matrix
 
 
 def get_random_def():
-    return "Dummy def"
-    # return ml_module.get_random_def()  # todo import
+    definition = ml_module.get_random_object()
+    return definition
 
 
 @socketio.on('connect')
@@ -50,7 +61,7 @@ def start(data):
         return '-1'
 
     target = get_random_def()
-    room_targets[room_name] = target
+    room_map[room_name]['definition'] = target
     emit(CHALLENGE, {DEFINITION: target}, room=room_name)
 
 
@@ -60,43 +71,43 @@ def client_join(data):
     session_id = request.sid
     try:
         room_name = data[ROOM_ID]  # Get client room id
+        player_name = data[PLAYER_NAME]
     except KeyError:
         print("bad request format")
         return '-1'
     join_room(room=room_name, sid=session_id)  # Add client to its room
 
-    if session_id not in room_map:
-        room_map[session_id] = []
-    room_map[session_id].append(session_id)  # Save in room map
-    print(session_id + ' has entered the room ' + str(room_name))
-
-
-def check_answer(answer, room):
-    target = room_targets[room]
-    # Todo check if answer is close enough to target
-    pass
+    if room_name not in room_map:
+        room_map[room_name] = {
+            'players': [],
+            'definition': None
+        }
+    room_map[room_name]['players'].append({'session_id': session_id, 'player_name': player_name})  # Save in room map
+    print(player_name + ' has entered the room ' + str(room_name))
 
 
 @socketio.on(IDENTIFY)
 def identify_image(data):
     """identify user image"""
     try:  # Parse request
-        matrix = data[IMAGE_MATRIX]
-        user_name = data[USERNAME]
+        draw_object = data[DRAW_OBJECT]
+        user_name = data[PLAYER_NAME]
+        room_name = data[ROOM_ID]
     except KeyError:
         print("bad request format")
         return '-1'
 
+    print(data)
     over = False
-    room_name = find_room(user_name=user_name)
-    if room_name is None:
-        print("bad username")
+    room = room_map[room_name]
+    if room is None:
+        print("bad room name" + room_name)
         return '-1'
 
-    # answer = ml_module.predict(matrix)  # todo import
-    # over = check_answer(answer, room_name)  # Todo implement
-
-    if over:  # Announce game over
+    generated_matrix = create_matrix(draw_object)
+    # success = ml_module.predict(generated_matrix, room_map[room_name]['definition'])
+    success = True
+    if success:  # Announce game over
         data = {WINNER: user_name}
         emit(GAME_OVER, data, room=room_name)
 
