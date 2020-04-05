55b5eb9338d5c44dbf0280156c8d89b4a639daed
EladCoding <eladshoham93@gmail.com>
2020-04-05 16:50:52 +0300

    comment debugging code

--- MLpart.py
+++ MLpart.py
@@ -1,14 +1,13 @@
 import numpy as np
 from skimage.transform import resize
-from PIL import Image
-import matplotlib.pyplot as plt
-import pathlib
-import os
 from tensorflow.keras.models import load_model
 from keras import metrics
 import csv
 import random
-from skimage import img_as_bool
+# from PIL import Image
+# import matplotlib.pyplot as plt
+# import pathlib
+# import os
 
 
 IMG_WIDTH = 28
@@ -51,8 +50,8 @@ class Recognize():
             x = resize(x, (IMG_WIDTH, IMG_HEIGHT))
             x[x > 0.1] *= 1.5
             x[x > 1] = 1
-            plt.imshow(x, cmap="gray")
-            plt.show()
+            # plt.imshow(x, cmap="gray")
+            # plt.show()
 
             # TODO check other ways to resize, like:
             # large image is shape (1, 128, 128)
@@ -97,67 +96,68 @@ def top_3_acc(y_true, y_pred):
     return metrics.top_k_categorical_accuracy(y_true, y_pred, k=3)
 
 
-def load_quickdraw_images(quickdraw_images_path, max_images=100):
-    x = np.load(quickdraw_images_path)[:max_images] # load all images
-    x = x.astype(np.float32) / 255. # scale images
-    # reshape to 28x28x1 grayscale image
-    x = x.reshape(x.shape[0], IMG_WIDTH, IMG_HEIGHT, PIXEL_DIM)
-    return x
-
-
-def test_prediction(rec_obj):
-    load_from_quickdraw_bananas = True
-    if load_from_quickdraw_bananas:
-        cur_dir_path = pathlib.Path().absolute()
-        bananas_rel_path = "dataset/full_numpy_bitmap_banana_example.npy"
-        bananas_abs_path = os.path.join(cur_dir_path, bananas_rel_path)
-        bananas = load_quickdraw_images(bananas_abs_path)
-        rand_banana_spot = random.randint(0, bananas.shape[0])
-        banana = bananas[rand_banana_spot]
-    else:
-        banana = Image.open("C:/Users/EladShoham/Desktop/banana.jpg").convert('L')
-        banana = np.asarray(banana)
-        banana = banana.astype(np.float32) / 255. # scale images
-
-    banana_img = banana.squeeze()
-    plt.imshow(banana_img, cmap='gray')
-    plt.show()
-    banana_prediction = rec_obj.predict_specific_label(banana, rec_obj.definitions_to_labels["banana"])
-    print("banana prediction: %s" % (banana_prediction))
-    max_label, max_prediction = rec_obj.predict_max_label(banana)
-    print("max label: %s. prediction: %s" % (max_label, max_prediction))
-    classify_specific_definition = rec_obj.classify_specific_definition(banana, "banana")
-    print(classify_specific_definition)
-
-
-def get_default_model():
-    return get_20_10000_model()
-
-
-def get_20_10000_model():
-    cur_dir_path = pathlib.Path().absolute()
-    model_rel_path = "models/20/10000/model.h5"
-    labels_rel_path = "models/20/10000/labels.csv"
-    model_abs_path = os.path.join(cur_dir_path, model_rel_path)
-    labels_abs_path = os.path.join(cur_dir_path, labels_rel_path)
-    rec_obj = Recognize(model_abs_path, labels_abs_path)
-    return rec_obj
-
-
-def get_345_5000_model():
-    cur_dir_path = pathlib.Path().absolute()
-    model_rel_path = "models/345/5000/model.h5"
-    labels_rel_path = "models/345/5000/labels.csv"
-    model_abs_path = os.path.join(cur_dir_path, model_rel_path)
-    labels_abs_path = os.path.join(cur_dir_path, labels_rel_path)
-    rec_obj = Recognize(model_abs_path, labels_abs_path)
-    return rec_obj
-
-
-def main():
-    rec_obj = get_default_model()
-    test_prediction(rec_obj)
-
-
-if __name__ == '__main__':
-    main()
+# def load_quickdraw_images(quickdraw_images_path, max_images=100):
+#     x = np.load(quickdraw_images_path)[:max_images] # load all images
+#     x = x.astype(np.float32) / 255. # scale images
+#     # reshape to 28x28x1 grayscale image
+#     x = x.reshape(x.shape[0], IMG_WIDTH, IMG_HEIGHT, PIXEL_DIM)
+#     return x
+#
+#
+# def test_prediction(rec_obj):
+#     load_from_quickdraw_bananas = True
+#     if load_from_quickdraw_bananas:
+#         cur_dir_path = pathlib.Path().absolute()
+#         bananas_rel_path = "dataset/full_numpy_bitmap_banana_example.npy"
+#         bananas_abs_path = os.path.join(cur_dir_path, bananas_rel_path)
+#         bananas = load_quickdraw_images(bananas_abs_path)
+#         rand_banana_spot = random.randint(0, bananas.shape[0])
+#         banana = bananas[rand_banana_spot]
+#     else:
+#         banana = Image.open("C:/Users/EladShoham/Desktop/banana.jpg").convert('L')
+#         banana = np.asarray(banana)
+#         banana = banana.astype(np.float32) / 255. # scale images
+#
+#     banana_img = banana.squeeze()
+#     plt.imshow(banana_img, cmap='gray')
+#     plt.show()
+#     banana_prediction = rec_obj.predict_specific_label(banana, rec_obj.definitions_to_labels["banana"])
+#     print("banana prediction: %s" % (banana_prediction))
+#     max_label, max_prediction = rec_obj.predict_max_label(banana)
+#     print("max label: %s. prediction: %s" % (max_label, max_prediction))
+#     classify_specific_definition = rec_obj.classify_specific_definition(banana, "banana")
+#     print(classify_specific_definition)
+#
+#
+# def get_default_model():
+#     return get_20_10000_model()
+#
+#
+# def get_20_10000_model():
+#     cur_dir_path = pathlib.Path().absolute()
+#     model_rel_path = "models/20/10000/model.h5"
+#     labels_rel_path = "models/20/10000/labels.csv"
+#     model_abs_path = os.path.join(cur_dir_path, model_rel_path)
+#     labels_abs_path = os.path.join(cur_dir_path, labels_rel_path)
+#     rec_obj = Recognize(model_abs_path, labels_abs_path)
+#     return rec_obj
+#
+#
+# def get_345_5000_model():
+#     cur_dir_path = pathlib.Path().absolute()
+#     model_rel_path = "models/345/5000/model.h5"
+#     labels_rel_path = "models/345/5000/labels.csv"
+#     model_abs_path = os.path.join(cur_dir_path, model_rel_path)
+#     labels_abs_path = os.path.join(cur_dir_path, labels_rel_path)
+#     rec_obj = Recognize(model_abs_path, labels_abs_path)
+#     return rec_obj
+#
+#
+# def main():
+#     rec_obj = get_default_model()
+#     test_prediction(rec_obj)
+#
+#
+# if __name__ == '__main__':
+#     main()
+#
